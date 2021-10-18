479e8a06a6c1ea38e730f9cc2d4d3c2356e28867
tryspidy <68438592+tryspidy@users.noreply.github.com>
2021-10-18 15:12:48 +0530

    Initial commit

---
+++ main.java
@@ -0,0 +1,29 @@
+JVM:
+1.	JVM is an abstract machine. 
+    Because it doesn't physically exist.
+2.	It is a very important part of both JDK
+     and JRE because it is inbuilt in both. 
+3.	Whatever Java program we want to run, goes into JVM. 
+4.	And JVM is responsible for loading,
+    verifying and executing the java program 
+    line by line hence it is also known as interpreter.
+    
+The Java Development Kit (JDK) is a software development 
+environment used for developing Java applications. 
+It includes:
+	Java Virtual Machine, 
+	Java Runtime Environment,
+	Loader, 
+	Java compiler, 
+	Documentation generator 
+	Archiver (jar), 
+	Other tools needed in Java development.
+    
+ Java Runtime Environment :
+	It physically exists. 
+	The Java Runtime Environment provides
+    the minimum requirements such as libraries
+    and Class Loader for executing a 
+    Java application on JVM. 
+	It consists of the Java Virtual Machine
+    core classes, and supporting files.    
